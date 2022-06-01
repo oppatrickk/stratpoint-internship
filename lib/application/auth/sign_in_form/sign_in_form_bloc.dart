fc348f8bc72a82c9569c06221ee86bb7bfcb85a3
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2022-06-01 10:51:49 +0800

    feat(login): Implemented Sign-In Form Logic

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -17,6 +17,7 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthFacade _authFacade;
 
   SignInFormBloc(this._authFacade);
+  // ! The implicitly invoked unnamed constructor from 'Bloc<SignInFormEvent, SignInFormState>' has required parameters.
 
   @override
   SignInFormState get InitialState => SignInFormState.initial();
@@ -25,6 +26,72 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   Stream<SignInFormState> mapEventToState(
     SignInFormEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.map(
+        // Email Changed
+        emailChanged: (e) async* {
+      yield state.copyWith(
+        emailAddress: EmailAddress(e.emailStr),
+        authFailureOrSuccessOption: none(),
+      );
+    },
+
+        // Password Changed
+        passwordChanged: (e) async* {
+      yield state.copyWith(
+        password: Password(e.passwordStr),
+        authFailureOrSuccessOption: none(),
+      );
+    },
+
+        // RegisterWithEmailAndPasswordPressed
+        registerWithEmailAndPasswordPressed: (e) async* {
+      yield* _performActionOnAuthFacadeWithEmailAndPassword(
+        _authFacade.registerWithEmailAndPassword,
+      );
+    },
+        // SignInWithEmailAndPasswordPressed
+        signInWithEmailAndPasswordPressed: (e) async* {
+      yield* _performActionOnAuthFacadeWithEmailAndPassword(
+        _authFacade.signInWithEmailAndPassword,
+      );
+    },
+
+        // SignInWithGooglePressed
+        signInWithGooglePressed: (e) async* {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureOrSuccessOption: none(),
+      );
+      final failureOrSuccess = await _authFacade.signInWithGoogle();
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureOrSuccessOption: some(failureOrSuccess),
+      );
+    });
+  }
+
+  Stream<SignInFormState> _performActionOnAuthFacadeWithEmailAndPassword(
+    Future<Either<AuthFailure, Unit>> Function({required EmailAddress emailAddress, required Password password}) forwardedCall,
+  ) async* {
+    Either<AuthFailure, Unit> failureOrSuccess;
+
+    final isEmailValid = state.emailAddress.isValid();
+    final isPasswordValid = state.password.isValid();
+
+    if (isEmailValid && isPasswordValid) {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureOrSuccessOption: none(),
+      );
+
+      failureOrSuccess = await forwardedCall(emailAddress: state.emailAddress, password: state.password);
+    }
+
+    yield state.copyWith(
+      isSubmitting: false,
+      showErrorMessages: true,
+      authFailureOrSuccessOption: optionOf(failureOrSuccess),
+      // ! The non-nullable local variable 'failureOrSuccess' must be assigned before it can be used.
+    );
   }
 }
